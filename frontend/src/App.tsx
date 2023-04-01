ad0251da72287743a035d2c8184bb738516bca19
hmaryn@byu.edu <95374695+hmaryn11@users.noreply.github.com>
2023-04-01 16:54:00 -0600

    It's Janky but it works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import AddMovieID from './Movies';
+import Movie from './Movies';
+import Podcast from './Podcast';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <AddMovieID />
+      <Home />
+      <Movie />
+      <Podcast />
     </div>
   );
 }
